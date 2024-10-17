a890eb96a42174458fdaf1d6b892f627b6791921
candyburst <townshipiamgroot@gmail.com>
2024-10-17 08:37:12 +0200

    Update invite.js

--- src/services/invite.js
+++ src/services/invite.js
@@ -10,12 +10,12 @@ class InviteClass {
         return data;
       } else {
         throw new Error(
-          `Lấy thông tin invite balance thất bại: ${data.message}`
+          `Failed to retrieve invite balance information: ${data.message}`
         );
       }
     } catch (error) {
       user.log.logError(
-        `Lấy thông tin invite balance thất bại: ${error.response?.data?.message}`
+        `Failed to retrieve invite balance information: ${error.response?.data?.message}`
       );
       return 0;
     }
@@ -26,17 +26,17 @@ class InviteClass {
       const { data } = await user.http.post(3, "friends/claim", {});
       if (data) {
         user.log.log(
-          `Claim điểm giới thiệu thành công, nhận được: ${colors.green(
+          `Successfully claimed referral points, received: ${colors.green(
             data?.claimBalance + user.currency
           )}`
         );
         return true;
       } else {
-        throw new Error(`Claim điểm giới thiệu thất bại: ${data.message}`);
+        throw new Error(`Failed to claim referral points: ${data.message}`);
       }
     } catch (error) {
       user.log.logError(
-        `Claim điểm giới thiệu thất bại: ${error.response?.data?.message}`
+        `Failed to claim referral points: ${error.response?.data?.message}`
       );
       return false;
     }
