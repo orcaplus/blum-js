65509f751b5c05c1ff5edf7cc30d104be8ce4c73
orcaplus <158179215+orcaplus@users.noreply.github.com>
2024-10-18 17:49:38 +0700

    Update invite.js

--- src/services/invite.js
+++ src/services/invite.js
@@ -10,12 +10,12 @@ class InviteClass {
         return data;
       } else {
         throw new Error(
-          `Failed to retrieve invite balance information: ${data.message}`
+          `Failed to get invite balance information: ${data.message}`
         );
       }
     } catch (error) {
       user.log.logError(
-        `Failed to retrieve invite balance information: ${error.response?.data?.message}`
+        `Failed to get invite balance information: ${error.response?.data?.message}`
       );
       return 0;
     }
@@ -26,17 +26,17 @@ class InviteClass {
       const { data } = await user.http.post(3, "friends/claim", {});
       if (data) {
         user.log.log(
-          `Successfully claimed referral points, received: ${colors.green(
+          `Claim referral points successfully, received: ${colors.green(
             data?.claimBalance + user.currency
           )}`
         );
         return true;
       } else {
-        throw new Error(`Failed to claim referral points: ${data.message}`);
+        throw new Error(`Referral Point Claim Failed: ${data.message}`);
       }
     } catch (error) {
       user.log.logError(
-        `Failed to claim referral points: ${error.response?.data?.message}`
+        `Referral Point Claim Failed: ${error.response?.data?.message}`
       );
       return false;
     }
